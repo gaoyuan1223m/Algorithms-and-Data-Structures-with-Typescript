28c651154b364fadad19bd8d4ab8167c403cccba
Yuan Gao <ygao17@slb.com>
2019-11-12 13:26:45 +0800

    [test] ut for dynamic array

--- test/DataStructure/dynamic-array/dynamic-array.test.ts
+++ test/DataStructure/dynamic-array/dynamic-array.test.ts
@@ -1,23 +1,34 @@
-import { DynamicArray } from "../../../src/DataStructure/dynamic-array/dynamic-array";
-import { IArray } from "../../../src/DataStructure/dynamic-array/dynamic-array-interface";
-import { IComparable } from "../../../src/Interface/IComparable";
-
-const dynamicArray: IArray<number> = new DynamicArray<number>(7);
-
-dynamicArray
-    .insert(4, 3)
-    .insert(6, 1)
-    .insert(7, 0)
-    // .update(9, 5)
-    .insert(8, 2)
-    .insert(11, 4)
-    .insert(15, 6)
-    // full
-    // .insert(100, 2)
-    // .remove(0)
+import { DynamicArray } from "@DataStructure/dynamic-array/dynamic-array";
+import { IArray } from "@DataStructure/dynamic-array/dynamic-array-interface";
 
 
-console.log(dynamicArray.size);
-console.log(dynamicArray.get(-4));
+describe(`Test for Dynamic Array`, () => {
 
-dynamicArray.print();
+    const dynamicArray: IArray<number> = new DynamicArray<number>(7);
+    const seed: number[] = [7, 6, 8, 4, 11, 9, 15];
+
+    beforeAll(() => {
+        for (const [index, value] of seed.entries()) {
+            dynamicArray.insert(value, index)
+        }
+    });
+
+    it(`#Get size of the Dynamic Array`, () => {
+        expect(dynamicArray.size).toBe(seed.length);
+    });
+
+    it(`#Get value by positive index`, () => {
+        expect(dynamicArray.get(0)).toBe(7);
+        expect(dynamicArray.get(6)).toBe(15);
+    });
+
+    it(`#Get value by negative index`, () => {
+        expect(dynamicArray.get(-1)).toBe(15);
+        expect(dynamicArray.get(-7)).toBe(7);
+    });
+})
+
+// console.log(dynamicArray.size);
+// console.log(dynamicArray.get(-4));
+
+// dynamicArray.print();
