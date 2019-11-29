5c6bea2889fa0d46f41751d2f105f841e857c50a
Yuan Gao <ygao17@slb.com>
2019-11-29 09:56:38 +0800

    [fix]move comparable-entity to concrete

---
+++ src/Entity/concrate/comparable-entity.ts
@@ -0,0 +1,24 @@
+import { IComparable } from "./node_modules/@Interface/common/IComparable";
+import { IObject } from "./node_modules/@Interface/common/IObject";
+import { Comparison } from "./node_modules/@Utils/comparison";
+
+export class Avenger implements IComparable<Avenger>, IObject {
+
+    readonly id: string;
+    readonly name: string;
+    readonly age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    };
+
+    compareWith(value: Avenger): number {
+        if (this.name === value.name && this.age === value.age) return Comparison.EQUALS_TO;
+
+        return this.age < value.age ? Comparison.LESS_THAN : Comparison.BIGGER_THAN;
+    }
+
+    toString = (): string => `${this.name} with ${this.age} year${this.age > 1 ? 's' : ''} old`;
+
+}
