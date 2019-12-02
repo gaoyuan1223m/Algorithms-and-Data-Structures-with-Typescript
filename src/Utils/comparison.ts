60d1de4be99450ef8eea95417d97d5c82a59394f
Yuan Gao <ygao17@slb.com>
2019-12-02 17:53:15 +0800

    [Feature]add new compareFunction<T>

--- src/Utils/comparison.ts
+++ src/Utils/comparison.ts
@@ -35,32 +35,48 @@ export function isLarger(result: ComparisonResult): boolean {
     return result === ComparisonResult.LARGER_THAN;
 }
 
-export function isEqualOrLarger(result: ComparisonResult): boolean {
-    return result === ComparisonResult.EQUALS_TO || result === ComparisonResult.LARGER_THAN;
+export function isLargerOrEqual(result: ComparisonResult): boolean {
+    return result === ComparisonResult.LARGER_THAN || result === ComparisonResult.EQUALS_TO;
 }
 
 export function isLess(result: ComparisonResult): boolean {
     return result === ComparisonResult.LESS_THAN;
 }
 
-export function isEqualOrLess(result: ComparisonResult): boolean {
-    return result === ComparisonResult.EQUALS_TO || result === ComparisonResult.LESS_THAN;
+export function isLessOrEqual(result: ComparisonResult): boolean {
+    return result === ComparisonResult.LESS_THAN || result === ComparisonResult.EQUALS_TO;
 }
 
 
-export type ICompareFunc<T> = (a: T, b: T) => () => boolean;
-export function defaultComparison<T>(a: T, b: T) {
-    const isEqualTo = () => a === b;
-    const isLargerThan = () => a > b;
-    const isLessThan = () => a < b;
-    const isLargerOrEqualTo = () => a >= b;
-    const isLessOrEqualTo = () => a < b;
-    return {
-        isEqualTo,
-        isLargerThan,
-        isLargerOrEqualTo,
-        isLessThan,
-        isLessOrEqualTo
-    }
+export interface IComparisonKeys<T> {
+    isEqualTo: (value2: T) => boolean
+    isLargerThan: (value2: T) => boolean
+    isLargerOrEqualTo: (value2: T) => boolean
+    isLessThan: (value2: T) => boolean
+    isLessOrEqualTo: (value2: T) => boolean
+}
 
+export type ICompareFunc<T> = (value1: T) => IComparisonKeys<T>;
+
+export function valueTypeComparison<T>(value1: T): IComparisonKeys<T> {
+    return {
+        isEqualTo: (value2: T) => value1 === value2,
+        isLargerThan: (value2: T) => value1 > value2,
+        isLargerOrEqualTo: (value2: T) => value1 >= value2,
+        isLessThan: (value2: T) => value1 < value2,
+        isLessOrEqualTo: (value2: T) => value1 <= value2
+    }
+}
+
+const func: ICompareFunc<number> = valueTypeComparison
+console.log(func(4).isLargerOrEqualTo(2))
+
+export function referenceTypeComparison<T extends IComparable<T>>(value1: T): IComparisonKeys<T> {
+    return {
+        isEqualTo: (value2: T) => isEqual(value1.compareWith(value2)),
+        isLargerThan: (value2: T) => isLarger(value1.compareWith(value2)),
+        isLargerOrEqualTo: (value2: T) => isLargerOrEqual(value1.compareWith(value2)),
+        isLessThan: (value2: T) => isLess(value1.compareWith(value2)),
+        isLessOrEqualTo: (value2: T) => isLessOrEqual(value1.compareWith(value2))
+    }
 }
