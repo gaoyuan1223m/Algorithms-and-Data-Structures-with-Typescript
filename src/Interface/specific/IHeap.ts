494cc88205734cf320d3bf6b144bfb1425c9009f
247673441 <247673441@qq.com>
2020-01-19 14:34:05 +0800

    [feat]IHeap

---
+++ src/Interface/specific/IHeap.ts
@@ -0,0 +1,47 @@
+import { ICollectionBase } from "@Interface/common";
+import { ICompareFunc } from "@Utils/compare";
+
+
+export interface IHeap<T> extends ICollectionBase {
+
+    /**
+     * Return the Peak value of the HEAP without removing it
+     */
+    readonly peak: T;
+
+    /**
+     * *Add an element to the HEAP*
+     * @param value element to add to the HEAP
+     */
+    add(value: T): this;
+
+    /**
+     * *Remove the Peak element from the HEAP*
+     */
+    removePeak(): this;
+
+    /**
+     * 
+     * @param value element to replace the Peak element of the HEAP
+     */
+    replacePeakBy(value: T): this;
+}
+
+export interface IHeapConstructor {
+    new <T>(compare: ICompareFunc<T>): IHeap<T>
+}
+
+/**
+ * Implement HEAP by Dynamic Array since it's a complete Binary HEAP
+ * n is the number of elements
+ * i is index of the some element
+ * 
+ * i = 0, root node
+ * i > 0, its parent is ~~((i -1)/2)
+ * 
+ * if (2i + 1 <= n - 1), left node 2i + 1
+ * if (2i + 1 > n - 1), no left
+ * 
+ * if (2i + 2 <= n - 1), right node 2i + 2
+ * if (2i + 2 > n - 1), no right
+ */
