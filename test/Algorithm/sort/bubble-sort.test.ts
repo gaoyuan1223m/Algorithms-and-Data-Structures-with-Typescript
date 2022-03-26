3863916c1f0c5993c88251a5131b8b704d6e6d83
rickgao <rickgao@tencent.com>
2022-03-26 20:40:55 +0800

    test: bubble sort

---
+++ test/Algorithm/sort/bubble-sort.test.ts
@@ -0,0 +1,29 @@
+import { SortMethods } from "@Algorithm/sort/sort-methods";
+import { ArrayFactory } from "@DataStructure/array"
+import { ArrayTypes } from "@Utils/types";
+import { valueTypeComparison } from "@Utils/compare";
+import { IArray } from "@Interface/specific";
+
+describe(`Test for Bubble-sort`, () => {
+    let array: IArray<number>;
+
+    beforeAll(() => {
+        const capacity = 5
+        array = ArrayFactory.create(ArrayTypes.DYNAMIC, valueTypeComparison, capacity);
+        array
+            .append(5)
+            .append(7)
+            .append(6)
+            .append(3)
+            .append(1);
+
+    });
+
+    it(`#sort increasingly`, () => {
+        array
+            .sort(SortMethods.Bubble)
+            .print();
+
+        console.log(array);
+    });
+})
