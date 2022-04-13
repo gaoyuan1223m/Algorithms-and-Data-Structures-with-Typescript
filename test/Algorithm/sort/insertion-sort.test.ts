6f3410ed37c8def1e0318deff69a48daea7263e6
rickgao <rickgao@tencent.com>
2022-04-13 22:25:32 +0800

    test: insertion-sort

---
+++ test/Algorithm/sort/insertion-sort.test.ts
@@ -0,0 +1,30 @@
+import { SortMethods } from "@Algorithm/sort/sort-methods";
+import { ArrayFactory } from "@DataStructure/array"
+import { ArrayTypes } from "@Utils/types";
+import { valueTypeComparison } from "@Utils/compare";
+import { IArray } from "@Interface/specific";
+
+describe(`Test for Insertion-sort`, () => {
+    let array: IArray<number>;
+
+    beforeAll(() => {
+        const capacity = 8
+        array = ArrayFactory.create(ArrayTypes.STATIC, valueTypeComparison, capacity);
+        array
+            .append(5)
+            .append(2)
+            .append(6)
+            .append(3)
+            .append(1)
+            .append(7)
+            .append(9)
+            .append(10)
+            .print();
+    });
+
+    it(`#sort increasingly`, () => {
+        expect(array.sort(SortMethods.Insertion).isInIncreasingOrder).toBe(true);
+
+        array.print();
+    });
+})
