fcd29a217af445731240e2ff70801151445e8694
rickgao <rickgao@tencent.com>
2022-05-04 20:06:59 +0800

    test: add heap sort

--- test/Algorithm/array/sort.test.ts
+++ test/Algorithm/array/sort.test.ts
@@ -1,23 +1,20 @@
-import {
-  SortMethod,
-  generatePartiallyIncreasingArray,
-} from '@Algorithm/array';
+import { SortMethod, generatePartiallyIncreasingArray } from '@Algorithm/array';
 import { inRed } from "@Utils/emphasize";
-
 import { isSortedIncreasingly } from '../../Utils';
 
 describe('Test All Sort Methods', () => {
-  const length = 8000;
-  const randomOpt = { min: 0, max: 20000, from: 7000, to: length };
-  const originalDisorderedArray = generatePartiallyIncreasingArray(length, randomOpt);
-  console.log(inRed(`🚦 Array, length: ${length}, is ordered from "${randomOpt.from}" to "${randomOpt.to}"`));
+  it('1. len: 5000, partially ordered from "0" to the "1000"', () => {
+    const length = 30000;
+    const randomOpt = { min: 0, max: 100000, from: 0, to: 2000 };
+    const originalDisorderedArray = generatePartiallyIncreasingArray(length, randomOpt);
+    console.log(inRed(`🚦 Array, length: ${length}, is ordered from "${randomOpt.from}" to "${randomOpt.to}"`));
 
-  it('compare sort methods', () => {
     const { array: orderedArrayByBubble, ...restByBubble } = SortMethod.useBubbleSort([...originalDisorderedArray]);
     const { array: orderedArrayBySelect, ...restBySelect } = SortMethod.useSelectSort([...originalDisorderedArray]);
     const { array: orderedArrayByInsert, ...restByInsert } = SortMethod.useInsertSort([...originalDisorderedArray]);
     const { array: orderedArrayByMerge, ...restByMerge } = SortMethod.useMergeSort([...originalDisorderedArray]);
     const { array: orderedArrayByQuick, ...restByQuick } = SortMethod.useQuickSort([...originalDisorderedArray]);
+    const { array: orderedArrayByHeap, ...restByHeap } = SortMethod.useHeapSort([...originalDisorderedArray]);
 
     console.table({
       bubble: restByBubble,
@@ -25,6 +22,7 @@ describe('Test All Sort Methods', () => {
       insert: restByInsert,
       merge: restByMerge,
       quick: restByQuick,
+      heap: restByHeap,
       // ... TBC
     });
 
@@ -33,5 +31,6 @@ describe('Test All Sort Methods', () => {
     expect(isSortedIncreasingly(orderedArrayByInsert)).toBe(true);
     expect(isSortedIncreasingly(orderedArrayBySelect)).toBe(true);
     expect(isSortedIncreasingly(orderedArrayByBubble)).toBe(true);
+    expect(isSortedIncreasingly(orderedArrayByHeap)).toBe(true);
   });
 })
--- test/Algorithm/sort/heap-sort.test.ts
+++ test/Algorithm/sort/heap-sort.test.ts
@@ -5,28 +5,28 @@ import { valueTypeComparison } from "@Utils/compare";
 import { IArray } from "@Interface/specific";
 
 describe(`Test for Heap-sort`, () => {
-    let array: IArray<number>;
+  let array: IArray<number>;
 
-    beforeAll(() => {
-        const capacity = 8
-        array = ArrayFactory.create(ArrayTypes.STATIC, valueTypeComparison, capacity);
-        array
-            .append(5)
-            .append(7)
-            .append(6)
-            .append(3)
-            .append(10)
-            .append(7)
-            .append(9)
-            .append(1)
-            .print();
-    });
+  beforeAll(() => {
+    const capacity = 8
+    array = ArrayFactory.create(ArrayTypes.STATIC, valueTypeComparison, capacity);
+    array
+      .append(5)
+      .append(7)
+      .append(6)
+      .append(3)
+      .append(10)
+      .append(7)
+      .append(9)
+      .append(1)
+      .print();
+  });
 
-    it(`#sort increasingly`, () => {
-        array
-            .sort(SortMethods.HEAP)
-            .print();
+  it(`#sort increasingly`, () => {
+    array
+      .sort(SortMethods.HEAP)
+      .print();
 
-        expect(array.isInIncreasingOrder).toBe(true);
-    });
+    expect(array.isInIncreasingOrder).toBe(true);
+  });
 })
